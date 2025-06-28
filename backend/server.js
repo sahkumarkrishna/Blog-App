281c509b08691bf9545aa27973c470f140a97a14
Krishna Kumar <159722967+sahkumarkrishna@users.noreply.github.com>
2025-06-28 21:16:43 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import mongoose from "mongoose";
+
 import userRoute from "./routes/user.route.js";
 import blogRoute from "./routes/blog.route.js";
 import commentRoute from "./routes/comment.route.js";
 import cookieParser from 'cookie-parser';
+import connectDB from "./database/db.js";
 import cors from 'cors';
-import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,16 +18,6 @@ if (!process.env.MONGO_URI) {
     process.exit(1);
 }
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected successfully");
-    } catch (error) {
-        console.error("MongoDB connection error", error);
-        process.exit(1);
-    }
-};
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
@@ -36,16 +26,9 @@ app.use(cors({
     credentials: true
 }));
 
-const _dirname = path.resolve();
-
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/blog", blogRoute);
 app.use("/api/v1/comment", commentRoute);
-
-app.use(express.static(path.join(_dirname, "/frontend/dist")));
-app.get("*", (_, res) => {
-    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
-});
 
 connectDB().then(() => {
     app.listen(PORT, () => {
