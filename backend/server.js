6f9600eeb8067cdf1467ee459a71298f96936219
Krishna Kumar <159722967+sahkumarkrishna@users.noreply.github.com>
2025-06-28 22:15:19 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://blog-app-nine-theta-96.vercel.app",
     credentials: true
 }));
 
