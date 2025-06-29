701236a472e6ae9227485281cec38d6015a369b4
Krishna Kumar <159722967+sahkumarkrishna@users.noreply.github.com>
2025-06-29 09:21:53 +0530

    update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -32,7 +32,7 @@ const Login = () => {
     console.log(input);
 
     try {
-      const response = await axios.post(`http://localhost:5000/api/v1/user/login`, input, {
+      const response = await axios.post(`https://blog-app-rouge-sigma.vercel.app/api/v1/user/login`, input, {
         headers: {
           "Content-Type": "application/json"
         },
