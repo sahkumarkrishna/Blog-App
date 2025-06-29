b5f01c6c3cb8e7d3192ebf6b04ad0e54c20c6424
Krishna Kumar <159722967+sahkumarkrishna@users.noreply.github.com>
2025-06-29 09:23:57 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://blog-app-zosx.vercel.app",
     credentials: true
 }));
 
